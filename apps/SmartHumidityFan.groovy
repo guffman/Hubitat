5d082450abbb92c7dab3028a1dcfba4b307c8568
guffman <marc@chevis.me>
2020-10-10 09:38:00 -0500

    Update SmartHumidityFan.groovy

--- apps/SmartHumidityFan.groovy
+++ apps/SmartHumidityFan.groovy
@@ -17,6 +17,8 @@ definition(
     author: "Guffman",
     description: "Control a fan (switch) based on relative humidity.",
     category: "Convenience",
+    iconUrl: "",
+    iconX2Url: ""
     importUrl: "https://raw.githubusercontent.com/guffman/Hubitat/master/apps/SmartHumidityFan.groovy"
 )
 
