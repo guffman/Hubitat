fe47767db19f31a03d36cf5cfab82df4136530b4
guffman <marc@chevis.me>
2021-06-16 14:42:10 -0500

    Update AverageTemperatures.groovy

--- apps/AverageTemperatures.groovy
+++ apps/AverageTemperatures.groovy
@@ -5,9 +5,7 @@ definition(
     description: "Average some temperature sensors",
     category: "Convenience",
     iconUrl: "",
-    iconX2Url: "",
-    importUrl: "https://raw.githubusercontent.com/guffman/Hubitat/master/apps/AverageTemperatures.groovy"
-)
+    iconX2Url: "")
 
 preferences {
 	page(name: "mainPage")
@@ -16,8 +14,11 @@ preferences {
 def mainPage() {
 	dynamicPage(name: "mainPage", title: " ", install: true, uninstall: true) {
 		section {
-			input "thisName", "text", title: "Name this temperature averager", submitOnChange: true
-			if(thisName) app.updateLabel("$thisName")
+			input "thisName", "text", title: "Name this temperature averager", defaultValue: "Temperature Averager", submitOnChange: true
+            if(thisName) {
+                app.updateLabel("$thisName")
+                state.label = thisName
+            }
 			input "tempSensors", "capability.temperatureMeasurement", title: "Select Temperature Sensors", submitOnChange: true, required: true, multiple: true
 			paragraph "Enter weight factors and offsets"
 			tempSensors.each {
@@ -25,9 +26,10 @@ def mainPage() {
 				input "offset$it.id", "decimal", title: "$it Offset", defaultValue: 0.0, submitOnChange: true, range: "*..*", width: 3
 			}
 			input "useRun", "number", title: "Compute running average over this many sensor events:", defaultValue: 1, submitOnChange: true
-            input "lowClamp", "decimal", title: "Temperature clamp value low:", defaultValue: 50.0, required: true, submitOnChange: false
-            input "highClamp", "decimal", title: "Temperature clamp value high:", defaultValue: 100.0, required: true, submitOnChange: false
-            input "deltaClamp", "decimal", title: "Temperature clamp value delta:", defaultValue: 2.0, required: true, submitOnChange: false            
+            input "lowClamp", "decimal", title: "Temperature sensor clamp value low:", defaultValue: 50.0, required: true, submitOnChange: false
+            input "highClamp", "decimal", title: "Temperature sensor clamp value high:", defaultValue: 100.0, required: true, submitOnChange: false
+            input "deltaClamp", "decimal", title: "Temperature sensor clamp value delta:", defaultValue: 2.0, required: true, submitOnChange: false
+            
 			if(tempSensors) paragraph "Current sensor average is ${averageTemp()}°"
 			if(useRun > 1) {
 				initRun()
@@ -51,24 +53,21 @@ def initialize() {
 	if(!averageDev) averageDev = addChildDevice("hubitat", "Virtual Temperature Sensor", "AverageTemp_${app.id}", null, [label: thisName, name: thisName])
 	averageDev.setTemperature(averageTemp())
 	subscribe(tempSensors, "temperature", handler)
+    state.label = thisName
 }
 
 def initRun() {
 	def temp = averageTemp()
-//	if(!state.run) {
-		state.run = []
-		for(int i = 0; i < useRun; i++) state.run += temp
-//	}
-//    log.info "initRun: state.run = ${state.run}"
+	state.run = []
+	for(int i = 0; i < useRun; i++) state.run += temp
 }
 
 def averageTemp(run = 1) {
 	def total = 0
 	def n = 0
+    def avg = 0.0
     def averageDev = getChildDevice("AverageTemp_${app.id}")
     def currentTemp = averageDev.currentValue("temperature")
-    if (!lowClamp) lowClamp = 0.0
-    if (!highClamp) highClamp = 100.0
 	tempSensors.each {
         if (it.currentTemperature <=lowClamp || it.currentTemperature >= highClamp) log.warn "Temperature sensor ${it.label} reading ${it.currentTemperature} out of clamp range ${lowClamp}-${highClamp}"
 		def offset = settings["offset$it.id"] != null ? settings["offset$it.id"] : 0       
@@ -76,40 +75,49 @@ def averageTemp(run = 1) {
 		n += settings["weight$it.id"] != null ? settings["weight$it.id"] : 1
 	}
 	def result = total / (n = 0 ? tempSensors.size() : n)
-    
 	if(run > 1) {
 		total = 0
 		state.run.each {total += it}
 		result = total / run
 	}
+    def deltaResult = Math.abs(currentTemp - result)
+    
+    // Sensor average validity tests
+    avg = result.toDouble().round(1)
+    avgStr = String.format("%.1f", avg) + "°F"
+    avgStr = avgStr.trim()
+    
     if ((result > lowClamp) && (result < highClamp) && (Math.abs(currentTemp - result) < deltaClamp)) {
 		state.clamped = false
-        return result.toDouble().round(1)
-    } else {
-		state.clamped = true
-        log.warn "Temperature average clamped: ignoring calculated average value ${result}"
-        return currentTemp
+        updateAppLabel(avgStr, "green")
+        return avg
     }
+	
+    state.clamped = true
+    if (result <= lowClamp) {
+        log.warn "Temperature average clamped: calculated value ${avgStr} below ${lowClamp}"
+        updateAppLabel("Below Low Limit - Clamped", "orange", avgStr)
+    } else if (result >= highClamp) {
+        log.warn "Temperature average clamped: calculated value ${avgStr} above ${highClamp}"
+        updateAppLabel("Above High Limit - Clamped", "orange", avgStr)
+    } else if (Math.abs(currentHumid - result) >= deltaClamp) {
+        log.warn "Temperature average clamped: calculated value ${avgStr} changed more than ${deltaClamp}"
+        updateAppLabel("Above Delta Limit - Clamped", "orange", avgStr)
+    }    
+        return currentTemp
 }
 
 def handler(evt) {
 	def averageDev = getChildDevice("AverageTemp_${app.id}")
 	def avg = averageTemp()
-	def avgStr = ""
 	if(useRun > 1) {
-        //log.info "Event change before: state.run = ${state.run}"
 		state.run = state.run.drop(1) + avg
-        //log.info "Event change after: state.run = ${state.run}"
 		avg = averageTemp(useRun)
 	}
 	averageDev.setTemperature(avg)
-	avgStr = String.format("%.1f", avg) + "°F"
-    avgStr = avgStr.trim()
-//	log.info "Average sensor temperature = ${averageTemp()}°" + (useRun > 1 ? "    Running average is $avg°" : "")
-	state.clamped ? updateAppLabel("Clamped", "orange", "${avgStr},") : updateAppLabel("${avgStr}", "green")
 }
 
-def updateAppLabel(textStr, textColor, textPrefix = '') {
-    def str = (textPrefix = null) ? """<span style='color:$textColor'> ($textPrefix $textStr)</span>""" : """<span style='color:$textColor'> ($textStr)</span>"""
-    app.updateLabel(thisName + str)
+def updateAppLabel(textStr, textColor, def textPrefix=null) {
+    def str = (textPrefix != null) ? """<span style='color:$textColor'> ($textPrefix $textStr)</span>""" : """<span style='color:$textColor'> ($textStr)</span>"""
+    app.updateLabel(state.label + str)
 }
