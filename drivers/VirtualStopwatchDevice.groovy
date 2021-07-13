8671adb1b3f911b8867f29e5f1b5bbecea7eaa6b
guffman <marc@chevis.me>
2021-07-13 09:15:07 -0500

    Update VirtualStopwatchDevice.groovy
    Revised statistics reset logic

--- drivers/VirtualStopwatchDevice.groovy
+++ drivers/VirtualStopwatchDevice.groovy
@@ -11,7 +11,7 @@
  *      Button 1 pushed - starts the stopwatch
  *      Button 2 pushed - stops the stopwatch
  *      Button 3 pushed - cancels the running stopwatch
- *.     Button 4 pushed - pause (not implemented)
+ *      Button 4 pushed - resets the stopwatch statistics and related counters
  *
  *  Switch capabilities are added for convenience:
  *      On  - starts the stopwatch
@@ -37,6 +37,7 @@
  *    2021-06-08  Marc Chevis    Original
  *    2021-06-12  Marc Chevis    Added attribute updates while timer is running, button capability
  *    2021-06-23  Marc Chevis    Added some statistics attributes, switch capability, removed pause feature
+ *    2021-07-06  Marc Chevis    Revised statistics reset logic
  * 
  */
 
@@ -54,24 +55,24 @@ metadata {
         attribute "units", "string"
         attribute "sessionStartTime", "date"
         attribute "sessionStopTime", "date"
+        attribute "cycles", "number"
         
         // These attributes are used for event and time-related statistics across timing sessions, since a reset() command was issued
-        
         attribute "totalCycles", "number"
         attribute "totalElapsedTime", "number"
         attribute "minElapsedTime", "number"
         attribute "maxElapsedTime", "number"
-        attribute "avgCycleRate", "number"
         attribute "dutyCycle", "number"
         attribute "statsStartTime", "date"
+        attribute "statsStopTime", "date"
+        attribute "statsElapsedDays", "number"
         
-        command "reset", [[name:"Reset", description:"Reset all stopwatch statistics"]]
-        command "clear", [[name:"Clear", description:"Clear state and schedules"]]
+        command "reset", [[name:"Reset", description:"Reset all stopwatch statistics and update statistics attributes"]]
+        command "clear", [[name:"Clear", description:"Clear state values, schedules, and reset statistics"]]
     }   
     
     preferences {
         input name: "timeUnit", type: "enum", title: "Elapsed time units", options: [["sec":"Seconds"],["min":"Minutes"],["hr":"Hours"],["day":"Days"]], required: true, multiple: false, defaultValue: "min"
-        input name: "cycleRateUnit", type: "enum", title: "Cycle rate units", options: [["min":"per Minute"],["hr":"per Hour"],["day":"per Day"]], required: true, multiple: false, defaultValue: "day"
         input name: "updateInterval", type: "enum", title: "Stopwatch running time update interval:", options: [["5":"5 sec"],["10":"10sec"],["15":"15 sec"],["30":"30 sec"],["60":"1 min"]], required: true, multiple: false, defaultValue: "30"
         input "logEnable", "bool", title: "Enable debug logging?", defaultValue: false
     }                                                                
@@ -79,7 +80,7 @@ metadata {
 
 def push(button) {
     
-    // Button 1=start, 2=stop, 3=pause, 4=cancel
+    // Button 1=start, 2=stop, 3=pause, 4=reset stats
     
     logdebug("push: button ${button} pushed")
     if (button == 1) { 
@@ -89,7 +90,7 @@ def push(button) {
     } else if (button == 3) {
         cancel()
     } else if (button == 4) {
-        pause()
+        reset()
      } else {
         log.warning("Undefined button pushed: button ${button}")
     }
@@ -131,57 +132,39 @@ def stop() {
     if (device.currentValue("sessionStatus") == "running") {
         logdebug("stop: stopwatch entered stopped state")
         
-        // Capture stop times and update stopwatch state
+        // Capture stop time, elaspsed time and update stopwatch state
         
         state.stopTimeMs = now()
-
+        et = deltaTime(state.startTimeMs, state.stopTimeMs, timeUnit)
         sendEvent(name: "sessionStatus", value: "stopped", descriptionText: "Stopwatch stopped")
         sendEvent(name: "switch", value: "off", descriptionText: "Stopwatch stopped")
-
-        // Compute results for the recent timing session and update attributes.
-        logdebug("stop: state.startTimeMs=${state.startTimeMs}, state.stopTimeMs=${state.stopTimeMs}")
-        et = deltaTime(state.startTimeMs, state.stopTimeMs, timeUnit)
-        
-        // Create a map object for the event data property
+        logdebug("stop: state.startTimeMs=${state.startTimeMs}, state.stopTimeMs=${state.stopTimeMs}, et=${et}")
+  
+        // Create a map object for the elapsedTime event data property
         startTime = new Date(state.startTimeMs).format("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
         stopTime = new Date(state.stopTimeMs).format("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
-                stopTime = new Date(state.stopTimeMs).format("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
-
         def tsMap = [start: startTime, stop: stopTime]
-        logdebug("stop: et=${et} ${timeUnit}, tsMap = ${tsMap}")
         
         // Send the recent timing session attribute updates
         sendEvent(name: "runningTime", value: et, unit: timeUnit, descriptionText: "Elapsed time since stopwatch started")
         sendEvent(name: "elapsedTime", value: et, unit: timeUnit, descriptionText: "Session elapsed time updated", isStateChange: true, data: tsMap)
         sendEvent(name: "sessionStopTime", value: stopTime, descriptionText: "Session stop time updated")
 
-        // Increment the total cycles statistic and update the attribute
-        cyc_tot = device.currentValue("totalCycles") + 1
-        sendEvent(name: "totalCycles", value: cyc_tot, descriptionText: "Total cycles updated")
+        // Increment the total cycles counter - statistics state varable and the attribute
+        state.totalCycles = state.totalCycles + 1
+        sendEvent(name: "cycles", value: state.totalCycles, descriptionText: "Cycle count incremented")
+
+        // Update the elapsed time totalizer statistics
+        state.totalElapsedTime = state.totalElapsedTime + et
+        state.totalElapsedTimeMs = state.totalElapsedTimeMs + (state.stopTimeMs - state.startTimeMs)
         
-        // Compute the cycle rate statistic and update the attribute
-        cyc_rate = cycleRate(cyc_tot)
-        sendEvent(name: "avgCycleRate", value: cyc_rate, descriptionText: "Average cycle rate updated", unit: "per ${cycleRateUnit}")
-        
-        // Compute the elapsed time totalizer statistic and update the attribute
-        et_tot = (device.currentValue("totalElapsedTime") + et).round(4)
-        sendEvent(name: "totalElapsedTime", value: et_tot, descriptionText: "Total elapsed time updated", unit: timeUnit)
-        
-        // Compute the duty cycle statistic and update the attribute
-        state.statsElapsedTimeMs = state.statsElapsedTimeMs + (state.stopTimeMs - state.startTimeMs)
-        duty_cyc = (100 * state.statsElapsedTimeMs / (state.stopTimeMs - state.statsStartTimeMs)).toInteger() 
-        sendEvent(name: "dutyCycle", value: duty_cyc, descriptionText: "Duty cycle updated", unit: "%")
-        
-        // Compute the maximum elapsed time statistic and update the attribute
-        et_max = device.currentValue("maxElapsedTime") 
-        if (et > et_max) sendEvent(name: "maxElapsedTime", value: et, descriptionText: "Maximum elapsed time updated", unit: timeUnit)
-        
-        // Compute the minimum elapsed time statistic and update the attribute
-        et_min = device.currentValue("minElapsedTime") 
-        if (et_min > 0) {
-            if (et < et_min) sendEvent(name: "minElapsedTime", value: et, descriptionText: "Minimum elapsed time updated", unit: timeUnit)
-        } else {
-            sendEvent(name: "minElapsedTime", value: et, descriptionText: "Minimum elapsed time updated", unit: timeUnit)
+        // Compute the minimum and maximum elapsed time statistics 
+        if (et > state.maxElapsedTime) state.maxElapsedTime = et
+        if (et < state.minElapsedTime) state.minElapsedTime = et
+        if (state.minElapsedTime == 0) {
+            state.minElapsedTime = et
+        } else if (et < state.minElapsedTime) {
+            state.minElapsedTime = et
         }
       
     } else {
@@ -205,21 +188,46 @@ def pause() {
 }
 
 def reset() {
-
-    // Initialize statistics attributes/state variables
     
-    state.statsElapsedTimeMs = 0
-    state.statsStartTimeMs = now()
-    statsStartTime = new Date(state.statsStartTimeMs).format("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    // Capture timestamps, compute interval statistics, clean up some precision
+    statsStopTimeMs = now()
+    statsStopTime = new Date(statsStopTimeMs).format("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    days = deltaTime(state.statsStartTimeMs, statsStopTimeMs, "day")
+    tot_et = state.totalElapsedTime
+    Long dc = (100L * state.totalElapsedTimeMs) / (statsStopTimeMs - state.statsStartTimeMs)
+    Integer dcPct = dc.toInteger()
 
-    sendEvent(name: "statsStartTime", value: statsStartTime, descriptionText: "Statistics reset datetime")
-    sendEvent(name: "totalCycles", value: 0, descriptionText: "Statistics reset totalCycles set to 0")
-    sendEvent(name: "totalElapsedTime", value: 0, descriptionText: "Statistics reset totalElapsedTime set to 0", unit: timeUnit)
-    sendEvent(name: "minElapsedTime", value: 0, descriptionText: "Statistics reset minElapsedTime set to 0", unit: timeUnit)
-    sendEvent(name: "maxElapsedTime", value: 0, descriptionText: "Statistics reset maxElapsedTime set to 0", unit: timeUnit)
-    sendEvent(name: "avgCycleRate", value: 0, descriptionText: "Statistics reset avgCycleRate set to 0", unit: cycleRateUnit)
-    sendEvent(name: "dutyCycle", value: 0, descriptionText: "Statistics reset dutyCycle set to 0", unit: "%")
+    logdebug("reset: statsStartTime=${state.statsStartTime}, statsStopTime=${statsStopTime}, days=${days}, totalCycles=${state.totalCycles}, dutyCycle=${dcPct}")
+    
+    // Create a map object for the statistics events' timestamps data property
+    def tsMap = [start: state.statsStartTime, stop: statsStopTime, duration: days]
+    
+    // Send statistics events
+    sendEvent(name: "totalCycles", value: state.totalCycles, descriptionText: "Total cycles since ${state.statsStartTime}", data: tsMap)
+    sendEvent(name: "totalElapsedTime", value: tot_et, descriptionText: "Total elapsed time since ${state.statsStartTime}", unit: timeUnit, data: tsMap)
+    sendEvent(name: "dutyCycle", value: dcPct, descriptionText: "Duty cycle since ${state.statsStartTime}", unit: "%", data: tsMap)
+    sendEvent(name: "minElapsedTime", value: state.minElapsedTime, descriptionText: "Minimum elapsed time since ${state.statsStartTime}", unit: timeUnit, data: tsMap)
+    sendEvent(name: "maxElapsedTime", value: state.maxElapsedTime, descriptionText: "Maximum elapsed time since ${state.statsStartTime}", unit: timeUnit, data: tsMap)
+    sendEvent(name: "statsStopTime", value: statsStopTime, descriptionText: "Statistics interval ending datetime")
+    sendEvent(name: "statsStartTime", value: state.statsStartTime, descriptionText: "Statistics interval starting datetime")
+    sendEvent(name: "statsElapsedDays", value: days, descriptionText: "Statistics interval duration", unit: 'day')
+   
+    // Now that we've sent the statistics interval events, initialize the statistics state variables
+    resetStateStatistics()
+    
+    // Reset the cycles counter attribute
+    sendEvent(name: "cycles", value: 0, descriptionText: "Reset cycles counter")
+}
 
+def resetStateStatistics() {
+    state.totalCycles = 0
+    state.totalElapsedTime = 0
+    state.totalElapsedTimeMs = 0
+    state.minElapsedTime = 0
+    state.maxElapsedTime = 0
+    ct = now()
+    state.statsStartTimeMs = ct
+    state.statsStartTime = new Date(ct).format("yyyy-MM-dd'T'HH:mm:ss.SSSZ")    
 }
 
 def updateStopwatch() {
@@ -237,39 +245,27 @@ def updateStopwatch() {
 def deltaTime(begin, end, units) {
     
     // Compute net elapsed time (in specified time units) between begin and end (in msec)
-    logdebug("deltaTime: begin=${begin}, end=${end}, units=$units}")
-    dt = (end - begin).toDouble()
-    def et = 0.0
-  
+    Long dt = (end - begin) / 1000L
+    Double dtd = dt.toDouble()
+    
     switch(units)
         {
         case "sec":
-            et = (dt / 1000).round(1)
+            et = dt
             break
         case "min":
-            et = (dt / 1000 / 60).round(2)
+            et = (dtd / 60.0).round(2)
             break
         case "hr":
-            et = (dt / 1000 / 60 / 60).round(3)
+            et = (dtd / 3600.0).round(3)
             break
         case "day":
-            et = (dt / 1000 / 60 / 60 / 24).round(4)
+            et = (dtd / 86400.0).round(4)
             break
     }
+    
+    logdebug("deltaTime: begin=${begin}, end=${end}, units=${units}, dt=${dt}, dtd=${dtd}, et=${et}")
     return et
-}
-           
-def cycleRate(cycles) {
-    
-    // Compute elapsed time and cycle rate since last reset() command.
-
-    cyc_rate = 0.0
-    def dt = deltaTime(state.statsStartTimeMs, state.stopTimeMs, cycleRateUnit)
-    cyc_rate = (cycles / dt).round(3)
- 
-    logdebug("calcCycleRate: cycles=${cycles}, deltat=${dt}, cyc_rate=${cyc_rate}")
-    
-    return cyc_rate
 }
 
 def installed() {
@@ -277,8 +273,6 @@ def installed() {
     // Called when device created. 
     log.info "installed..."
     configure()
-    
-    
 }
 
 def configure() {
@@ -288,17 +282,15 @@ def configure() {
     
     state.startTimeMs = 0
     state.stopTimeMs = 0
+    resetStateStatistics()
     
     sendEvent(name: "sessionStatus", value: "stopped", descriptionText: "Stopwatch stopped")
     sendEvent(name: "switch", value: "off", descriptionText: "Stopwatch stopped")
     sendEvent(name: "runningTime", value: 0, unit: timeUnit, descriptionText: "Stopwatch elapsed time reset")
+    sendEvent(name: "cycles", value: 0, descriptionText: "Stopwatch cycle counter reset")
     sendEvent(name: "units", value: timeUnit, descriptionText: "Stopwatch time units set to ${timeUnit}")
-    
-    // Initialize stats
-    
-    reset()
-    
-    // Set up the periodic 'stopwatch running' update task
+
+    // Set up the periodic 'stopwatch running' update task and the statistics interval 'reset' task
     
     scheduleRefresh()
 }
@@ -318,7 +310,7 @@ def scheduleRefresh() {
             schedule("0 */1 * ? * *", refresh)
             break
     }
-    logdebug("scheduleRefresh: rescheduled for every ${updateInterval} sec")
+    logdebug("scheduleRefresh: stopwatch running updates rescheduled for every ${updateInterval} sec")
 }
 
 def initialize() {
@@ -333,13 +325,9 @@ def updated() {
     // If time units are changed, any related attribute values must be re-scaled appropriately
     
     if (timeUnit != device.currentValue("units")) {
-        // transform attribute values
+        // transform attribute values???
         sendEvent(name: "units", value: timeUnit, descriptionText: "Stopwatch time units changed to ${timeUnit}")
     }
-       
-    // Compute the cycle rate statistic and update the attribute
-    //cyc_rate = cycleRate(tot_cyc)
-    //sendEvent(name: "avgCycleRate", value: cyc_rate, descriptionText: "Average cycle rate time base changed, value recalculated", unit: "per ${cycleRateUnit}")
     
     unschedule()
     scheduleRefresh()
@@ -351,7 +339,6 @@ def refresh() {
 
 def clear() {
     stop()
-    reset()
     unschedule()
     state.clear()
 }
