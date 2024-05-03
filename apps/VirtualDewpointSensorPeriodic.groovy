a71740a00a5a9f1c1b4df770bd8cf2bd30157d07
guffman <marc@chevis.me>
2024-05-03 08:05:12 -0500

    Update VirtualDewpointSensorPeriodic.groovy

--- apps/VirtualDewpointSensorPeriodic.groovy
+++ apps/VirtualDewpointSensorPeriodic.groovy
@@ -21,7 +21,7 @@ definition(
     author: "Guffman",
     description: "Periodically calculate a dewpoint in degrees Farenheit, given a humidity and a temperature. Allow for time series smoothing using an exponential smoothing filter algorithm.",
     category: "Convenience",
-    importUrl: "https://raw.githubusercontent.com/guffman/Hubitat/master/apps/VirtualDewpointSensor.groovy",
+    importUrl: "https://raw.githubusercontent.com/guffman/Hubitat/master/apps/VirtualDewpointSensorPeriodic.groovy",
     iconUrl: "",
     iconX2Url: "")
 
